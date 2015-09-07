cc1ff87163228cb0096a82119fa9e3909584cd68
Darren Siegel <darrensiegel@Darrens-MBP.home>
2015-09-06 20:40:13 -0400

    updated open house date

--- index.php
+++ index.php
@@ -586,7 +586,7 @@
 
 <div class="row margin-40">
 <h2><center>Open House<center></h2>
-  <h3><center>Sunday, September 6th<center></h3>
+  <h3><center>Sunday, September 13th<center></h3>
       <h3><center>1 - 4pm<center></h3>
 </div>
 
