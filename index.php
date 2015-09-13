8f54283df6d5395a64acfeb307b1e7ad53e73527
Darren Siegel <darrensiegel@Darrens-MBP.home>
2015-09-13 17:19:37 -0400

    updated open house date for 20th

--- index.php
+++ index.php
@@ -592,8 +592,8 @@ header("Pragma: no-cache");
 
 <div class="row margin-40">
 <h2><center>Open House<center></h2>
-  <h3><center>September 13th<center></h3>
-      <h3><center>1 - 4pm<center></h3>
+  <h3><center>September 20th<center></h3>
+      <h3><center>Noon - 4pm<center></h3>
 </div>
 
 
