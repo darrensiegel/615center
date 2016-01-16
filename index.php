65c24f5c3731c821a2f6d6b42f4e69df07cab5df
Darren Siegel <darrensiegel@Darrens-MBP.home>
2016-01-16 09:12:02 -0500

    updated to restore price

--- index.php
+++ index.php
@@ -586,14 +586,14 @@ header("Pragma: no-cache");
     </div>
 
 <div class="row margin-40">
-<h2><center>Asking Price: $399,000<center></h2>
+<h2><center>Asking Price: $419,000<center></h2>
 </div>
 
 
 <div class="row margin-40">
-<h2><center>Open House<center></h2>
-  <h3><center>November 22nd<center></h3>
-      <h3><center>Noon - 3pm<center></h3>
+<h2><center><center></h2>
+  <h3><center><center></h3>
+      <h3><center><center></h3>
 </div>
 
 
